bda3ba76a25b87d3533e8ca7c1c18196423e39db
Brett <brettrheenan@gmail.com>
2016-06-16 12:33:51 +0200

    Done.

--- tests/Routes.spec.js
+++ tests/Routes.spec.js
@@ -16,4 +16,36 @@ describe('Routes', function () {
 			expect(state.url).toEqual('/settings');
 		});
 	});
+
+	describe('User', function() {
+		var state;
+
+		it('should have the correct url', function() {
+			state = $state.get('settings.user');
+
+			expect(state.url).toEqual('/user')
+		});
+
+		it('should render the correct template', function() {
+			expect(state.templateUrl).toEqual('views/settings/user.html')
+		});
+	});
+
+	describe('Notifications', function() {
+		var state;
+
+		it('should have the correct url', function() {
+			state = $state.get('settings.notifications');
+
+			expect(state.url).toEqual('/notifications')
+		});
+
+		it('should render the correct template', function() {
+			expect(state.templateUrl).toEqual('views/settings/notifications.html')
+		})
+
+		it('should call the correct controller', function() {
+			expect(state.controller).toEqual('NotificationsSettingsController')
+		})
+	})
 });
